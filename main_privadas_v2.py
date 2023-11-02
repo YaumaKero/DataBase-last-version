716028e9309a6fd21360173845ba92a2e5b72ae2
Carlota099 <148442892+Carlota099@users.noreply.github.com>
2023-11-02 11:38:19 +0100

    Create main_privadas_v2.py
    S'ha adaptat a les transaccions d'empreses. No ordenem per any però ara treu les transaccions d'abans de 2015

---
+++ main_privadas_v2.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import os
+import json
+import csv
+import convertidor
+
+# mainprivadas_v1-------------------------------------
+# Crea el csv total
+# Pone un ID
+# passa a  minusculas para poner todos los auto en automobilistica
+# elimina loss años antes del 2015
+
+# Nombre de la carpeta JSON de entrada y CSV de salida
+directorio_json = 'C:/Users/Carlota/OneDrive/Escritorio/pae/carpeta_json'
+archivos_json = [archivo for archivo in os.listdir(directorio_json) if archivo.endswith('.json')]
+
+# Lista para almacenar DataFrames de los archivos CSV
+dataframes = []
+
+# Itera a través de los archivos JSON y los convierte a CSV
+for archivo_json in archivos_json:
+    convertidor.convertir_json_a_csv(directorio_json, archivo_json)
+
+# Directorio que contiene los archivos CSV
+directorio_csv = 'C:/Users/Carlota/OneDrive/Escritorio/pae'
+
+# Itera a través de los archivos en el directorio
+for archivo in os.listdir(directorio_csv):
+    if archivo.endswith('.csv'):  # Asegúrate de que sean archivos CSV
+        ruta_completa = os.path.join(directorio_csv, archivo)
+        df = pd.read_csv(ruta_completa)
+        dataframes.append(df)
+        print(f"Metido en la database: '{archivo}'.")
+
+
+# Combina los DataFrames utilizando la función concat
+df_combined = pd.concat(dataframes, ignore_index=True)
+
+# Elimina las transacciones con "Año" previo a 2015
+df_combined = df_combined[df_combined["Año"] >= 2015]
+
+#lineas que tengan todos los valores nulos las elimina
+df_combined = df_combined.dropna(how='all')
+
+#Las lineas que sean iguales las elimina
+df_combined = df_combined.drop_duplicates(subset=["Compañía adquiridora","Compañía adquirida","Precio","Porcentaje adquirido","Año"])
+
+# Pone IDs
+#df_combined = df_combined.drop(columns=["ID"])
+df_combined["ID"] = range(1, len(df_combined) + 1)
+# Poner la columna ID al principio
+cols = df_combined.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+df_combined = df_combined[cols]
+
+# Guarda el DataFrame combinado en un nuevo archivo CSV
+df_combined.to_csv("BasedatosPrivados.csv", index=False)
+
+dbcsv = 'C:/Users/Carlota/OneDrive/Escritorio/pae/BasedatosPrivados.csv'
+
+# Cargar el archivo CSV en un DataFrame
+data = pd.read_csv(dbcsv)
+
+# Guardar el DataFrame modificado en un nuevo archivo CSV
+data.to_csv('BasedatosPrivados.csv', index=False)
+
+# Imprime el DataFrame combinado
+print("JUNTADO")
