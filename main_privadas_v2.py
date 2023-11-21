5319f1388466b6c79cf96b0b580fe629ce6f5b58
Carlota099 <148442892+Carlota099@users.noreply.github.com>
2023-11-21 12:30:25 +0100

    Add files via upload

--- main_privadas_v2.py
+++ main_privadas_v2.py
@@ -4,12 +4,6 @@ import json
 import csv
 import convertidor
 
-# mainprivadas_v1-------------------------------------
-# Crea el csv total
-# Pone un ID
-# passa a  minusculas para poner todos los auto en automobilistica
-# elimina loss años antes del 2015
-
 # Nombre de la carpeta JSON de entrada y CSV de salida
 directorio_json = 'C:/Users/Carlota/OneDrive/Escritorio/pae/carpeta_json'
 archivos_json = [archivo for archivo in os.listdir(directorio_json) if archivo.endswith('.json')]
@@ -29,6 +23,14 @@ for archivo in os.listdir(directorio_csv):
     if archivo.endswith('.csv'):  # Asegúrate de que sean archivos CSV
         ruta_completa = os.path.join(directorio_csv, archivo)
         df = pd.read_csv(ruta_completa)
+
+        # Reemplazar valores vacíos o nulos en la columna "Percent. acq." con "100%"
+        df["Percent. acq."] = df["Percent. acq."].fillna("100%")
+
+        # Convertir la columna "Year" a enteros
+        df["Year"] = df["Year"].fillna(1)
+        df["Year"] = df["Year"].astype(int)
+
         dataframes.append(df)
         print(f"Metido en la database: '{archivo}'.")
 
@@ -36,14 +38,32 @@ for archivo in os.listdir(directorio_csv):
 # Combina los DataFrames utilizando la función concat
 df_combined = pd.concat(dataframes, ignore_index=True)
 
-# Elimina las transacciones con "Año" previo a 2015
-df_combined = df_combined[df_combined["Año"] >= 2015]
+# Elimina las transacciones con "Year" previo a 2015
+df_combined = df_combined[df_combined["Year"] >= 2015]
 
 #lineas que tengan todos los valores nulos las elimina
 df_combined = df_combined.dropna(how='all')
 
 #Las lineas que sean iguales las elimina
-df_combined = df_combined.drop_duplicates(subset=["Compañía adquiridora","Compañía adquirida","Precio","Porcentaje adquirido","Año"])
+df_combined = df_combined.drop_duplicates(subset=["Purchaser","Company","Price","Percent. acq.","Year"])
+
+#Cuando hay dos lineas iguales pero en distintos años, coger el más reciente/eliminar el mas antiguo.
+df_combined.sort_values("Year", ascending=False, inplace=True)
+df_combined.drop_duplicates(subset=["Purchaser","Company","Price","Percent. acq."], keep='first', inplace=True)
+
+# Si hay 2 líneas iguales pero tienen el apartado price distinto
+price_difference_threshold = 0.2  # 20%
+
+for _, group in df_combined.groupby(["Purchaser", "Company", "Percent. acq."]):
+    if len(group) == 2:
+        prices = group["Price"].values
+        price_diff = abs(prices[0] - prices[1])
+        price_avg = max(prices)
+
+        if price_diff / price_avg <= price_difference_threshold:
+            df_combined.drop(group.index, inplace=True)
+            df_combined = pd.concat([df_combined, group.loc[group["Price"].idxmax()]], ignore_index=True)
+
 
 # Pone IDs
 #df_combined = df_combined.drop(columns=["ID"])
@@ -66,3 +86,25 @@ data.to_csv('BasedatosPrivados.csv', index=False)
 
 # Imprime el DataFrame combinado
 print("JUNTADO")
+
+
+# mainprivadas_v2-------------------------------------
+#afegirem que junti les diferents maneres d'esscriure un nom
+#hay que añadir el sector en algun momento
+#s'ha de poder actualitzar
+#posar només la transacció més recent?
+#twitter valoracio de l'empresa adquirida
+#dolares o euros
+
+
+
+
+
+
+#POSIBILITATS:
+#Irisbond, , Microsoft, 
+#MG o MG Motor Europe o 
+#Sabadell - Banco sabadell
+#Relypsa - Relypsa, INC.
+#PricewaterhouseCooper - PwC 
+#Boehringer Ingelheim
